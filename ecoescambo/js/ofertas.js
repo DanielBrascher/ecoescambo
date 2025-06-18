abb42fb04635ace6127fc1f9b4bb129ebcf0196c
Filipe Gil <filipe.79ce@gmail.com>
2025-06-17 22:35:34 -0300

    Inclusão do botão 'Rejeitar todas as ofertas'

--- ecoescambo/js/ofertas.js
+++ ecoescambo/js/ofertas.js
@@ -2,48 +2,77 @@ const urlParams = new URLSearchParams(window.location.search);
 const idProduto = urlParams.get("id_produto");
 
 if (!idProduto) {
- alert("Produto inválido.");
- window.location.href = "meus_produtos.html";
+  alert("Produto inválido.");
+  window.location.href = "meus_produtos.html";
 }
 
 $(document).ready(function () {
- carregarProduto();
- carregarInteressados();
+  carregarProduto();
+  carregarInteressados();
 
- $("#interessado").on("change", function () {
-   const idInteressado = $(this).val();
-   if (idInteressado) {
-     carregarProdutosInteressado(idInteressado);
-   } else {
-     $("#produtos-interessado").empty();
-   }
- });
+  $("#interessado").on("change", function () {
+    const idInteressado = $(this).val();
+    if (idInteressado) {
+      carregarProdutosInteressado(idInteressado);
+    } else {
+      $("#produtos-interessado").empty();
+    }
+  });
+
+  $("#rejeitarTodas").click(function () {
+    const idInteressado = $("#interessado").val();
+
+    if (!idInteressado) {
+      alert("Selecione um interessado antes de rejeitar.");
+      return;
+    }
+
+    if (confirm("Tem certeza que deseja rejeitar todas as ofertas deste interessado?")) {
+      $.ajax({
+        url: "api/rejeitar_todas.php",
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+          id_produto: idProduto,
+          id_interessado: idInteressado
+        }),
+        success: function (resposta) {
+          alert(resposta.mensagem);
+          carregarInteressados();
+          $("#produtos-interessado").empty();
+        },
+        error: function () {
+          alert("Erro ao rejeitar todas as ofertas.");
+        }
+      });
+    }
+  });
 });
 
 function carregarProduto() {
- $.get("api/produto.php", { id: idProduto }, function (produto) {
-   $("#produto-selecionado").html(`
-     <div class='card-produto'>
-       <img src='${produto.imagem}' />
-       <section class="info-produto">
-         <h3>${produto.nome}</h3>
-         <p>${produto.descricao}</p>
-       </section>
-     </div>
-   `);
- }, "json");
+  $.get("api/produto.php", { id: idProduto }, function (produto) {
+    $("#produto-selecionado").html(`
+      <div class='card-produto'>
+        <img src='${produto.imagem}' />
+        <section class="info-produto">
+          <h3>${produto.nome}</h3>
+          <p>${produto.descricao}</p>
+        </section>
+      </div>
+    `);
+  }, "json");
 }
 
 function carregarInteressados() {
- $.get("api/interessados_por_produto.php", { id_produto: idProduto }, function (dados) {
-   const select = $("#interessado");
-   select.empty();
-   select.append("<option value=''>-- selecione --</option>");
+  $.get("api/interessados_por_produto.php", { id_produto: idProduto }, function (dados) {
+    const select = $("#interessado");
+    select.empty();
+    select.append("<option value=''>-- selecione --</option>");
 
-   dados.forEach(i => {
-     select.append(`<option value='${i.id_usuario}'>${i.nome}</option>`);
-   });
- }, "json");
+    dados.forEach(i => {
+      select.append(`<option value='${i.id_usuario}'>${i.nome}</option>`);
+    });
+  }, "json");
 }
 
 function carregarProdutosInteressado(idUsuario) {
